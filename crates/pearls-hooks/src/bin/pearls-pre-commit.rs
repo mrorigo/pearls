ec2077145b84a1ea7a3949417771cd3281aa122a
mrorigo <mrorigo@gmail.com>
2026-02-07 11:37:03 +0100

    fix: removed redundant binaries

--- crates/pearls-hooks/src/bin/pearls-pre-commit.rs
+++
@@ -1,9 +0,0 @@
-// Rust guideline compliant 2026-02-06
-
-//! CLI entry point for Pearls pre-commit hook.
-
-fn main() -> anyhow::Result<()> {
-    let repo_path = std::env::current_dir()?;
-    pearls_hooks::pre_commit_hook(&repo_path)?;
-    Ok(())
-}
